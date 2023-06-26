f0851df00dd317315222992f6f03cc63826d1ef5
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-26 11:48:15 +0200

    ListCreateCommentAPIView added

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+
+from backend.comment.models import Comment
+from backend.comment.serializers import CommentSerializer
+
 
 # Create your views here.
+
+class ListCreateCommentAPIView(ListCreateAPIView):
+    model = Comment
+    serializer_class = CommentSerializer
