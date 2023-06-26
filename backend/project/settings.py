49eb9a4a72910ce350dc7c63ae2f53df9344d4e5
William Baldrige <liambald3@gmail.com>
2023-06-26 15:59:45 +0200

    user model and user profile model

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static-files/'
+STATIC_ROOT = 'static-files/' if DEBUG else '/static-files/'
+
+MEDIA_URL = 'media-files/'
+MEDIA_ROOT = 'media-files/' if DEBUG else '/media-files/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ backend/user_profile/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+def user_directory_path_avatar(instance, filename):
+    return f'avatar/{instance.id}/{filename}'
+
+def user_directory_path_banner(instance, filename):
+    return f'banner/{instance.id}/{filename}'
+
+class UserProfile():
+    user = models.OneToOneField(to='User', related_name='user_profile', blank=True, on_delete=models.CASCADE)
+    location = models.CharField(max_length=250, blank=True)
+    avatar = models.ImageField(upload_to=user_directory_path_avatar, blank=True, null=True)
+    banner = models.ImageField(upload_to=user_directory_path_banner, blank=True, null=True)
+    phone_no = models.CharField(max_length=30, null=True, blank=True)
+    about = models.TextField(max_length=250, null=True, blank=True)
