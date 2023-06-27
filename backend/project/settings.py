e4a31f6bdd45344b0157cc837cc42fcfeaa64da6
Stefan Lustenberger <chrott77@gmail.com>
2023-06-27 10:20:44 +0200

    added/prepared urls in project/urls and also added Token handling

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +145,14 @@ MEDIA_ROOT = 'media-files/' if DEBUG else '/media-files/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=6)
+}
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    # Admin page
+    path('backend/admin/', admin.site.urls),
+
+    # registration & authentication
+    path('backend/api/auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/api/auth/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
+
+    # path('backend/api/registration/', include('')),
+    # path('backend/api/password-reset/', include('')),
+
+    # API urls
+    # path('backend/api/users/', include('')),
+    path('backend/api/social/posts/', include('post.urls')),
+    # path('backend/api/social/comments/', include('')),
+    # path('backend/api/social/followers/', include('')),
+    # path('backend/api/social/friends/', include('')),
 ]
