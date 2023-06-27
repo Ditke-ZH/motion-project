49fada1a1e7c91435c6486d14edbcbc2459f21a5
William Baldrige <liambald3@gmail.com>
2023-06-27 10:16:46 +0200

    user model views/urls/serializers worked on

---
+++ backend/user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from backend.user.views import ViewAllFollowing, ViewAllUsers, ToggleFollowing, ViewAllFollowers, ViewOneUser
+
+urlpatterns = [
+    path('', ViewAllUsers.as_view()),
+    path('<int:id>/', ViewOneUser.as_view()),
+
+    path('toggle-follow/<int:id>/', ToggleFollowing.as_view()),
+
+    path('following/', ViewAllFollowing.as_view()),
+    path('followers/', ViewAllFollowers.as_view()),
+]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from drf_yasg.openapi import Response
-from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, UpdateAPIView, RetrieveAPIView
 from backend.user.serializers import UserSerializer
 
 User = get_user_model()
@@ -10,10 +10,18 @@ class ViewAllUsers(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    class ToggleFollowing(UpdateAPIView):
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
-        lookup_url_kwarg = 'id'
+
+class ViewOneUser(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_url_kwarg = 'id'
+
+
+class ToggleFollowing(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_url_kwarg = 'id'
+
     def patch(self, request, *args, **kwargs):
         is_followed_by_users = self.get_object()
         follows_users = self.request.user
@@ -24,14 +32,16 @@ class ViewAllUsers(ListAPIView):
             follows_users.is_followed_by_users.add(is_followed_by_users)
         return Response(self.get_serializer(is_followed_by_users).data)
 
-    class ViewAllFollowersUserFollowing(ListAPIView):
-        serializer_class = UserSerializer
 
-        def get_queryset(self):
-            return User.objects.filter(follows_users=self.request.user)
+class ViewAllFollowing(ListAPIView):
+    serializer_class = UserSerializer
 
-    class ViewAllFollowers(ListAPIView):
-        serializer_class = UserSerializer
+    def get_queryset(self):
+        return User.objects.filter(follows_users=self.request.user)
 
-        def get_queryset(self):
-            return User.objects.filter(is_followed_by_users=self.request.user)
+
+class ViewAllFollowers(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(is_followed_by_users=self.request.user)
