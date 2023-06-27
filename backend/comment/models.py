6629df2be711ff1c6bc70aa6e2a49546f0f6c662
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-27 11:24:20 +0200

    added relationship

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+from post.models import Post
 
-# Create your models here.
+User = get_user_model()
+
+
 class Comment(models.Model):
-    post = models.CharField(max_length=100)
-    commenting_user = models.CharField(max_length=100)
+    post = models.ManyToManyField(Post)
+    commenting_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Post id: {self.post} {self.text[:10]}... by user: {self.commenting_user}"
-
