a9d37d767bb0a6defc4cfb4a5ccc7c0ecfcd6df0
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-27 13:41:34 +0200

    commented out get queryset

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -11,7 +11,9 @@ class ListCreateCommentAPIView(ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+"""
     def get_queryset(self):
         user = self.request.user
         following_ids = user.following.values_list('id', flat=True)
         return Comment.objects.filter(creating_user_id__in=following_ids).order_by('-created_date')
+"""
