9b9c66a69d2eb805fd999a997e0c7da47874f231
Stefan Lustenberger <chrott77@gmail.com>
2023-06-27 11:29:46 +0200

    still trying to fix backend connectivity

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -22,7 +22,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
 
     # Admin page
-    path('backend/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # registration & authentication
     path('backend/api/auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
