a6a410804698bfa5cffe99f947f032d0978a89bf
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-27 14:36:24 +0200

    admin reverted back and with all options

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-User = get_user_model()
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    readonly_fields = ('date_joined',)
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2')}
+         ),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Groups', {'fields': ('groups',)}),
+    )
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('email',)
+
