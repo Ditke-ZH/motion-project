5d1765180e2bf563fcba7cf4c19248614d9e52a5
Stefan Lustenberger <chrott77@gmail.com>
2023-06-28 12:11:12 +0200

    registration works, but just one at a time...

--- backend/user_registration/admin.py
+++ backend/user_registration/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from user_registration.models import UserRegistration
+
+admin.site.register(UserRegistration)
