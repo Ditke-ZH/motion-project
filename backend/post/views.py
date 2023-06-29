ae5ed875f63944183ffa2d5c38ad68ed6fcad4f4
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-29 12:33:50 +0200

    linting

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,5 +1,4 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, GenericAPIView, \
-    UpdateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerIsAdminOrReadOnly
