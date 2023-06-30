820c4f957b99277ad3c9f564d8533dec286cced0
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-30 12:14:08 +0200

    comments for post_image

--- backend/post_image/views.py
+++ backend/post_image/views.py
@@ -1,14 +1,32 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import PostImage
+from .permissions import IsOwnerIsAdminOrReadOnly
 from .serializers import PostImageSerializer
 
 
 # Create your views here.
 class PostImageListCreateView(ListCreateAPIView):
+    """
+        get:
+        List of images linked with the post
+    """
     queryset = PostImage.objects.all().order_by('-created_date')
     serializer_class = PostImageSerializer
 
 
 class PostImageRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    """
+        get:
+        Get a specific post_image information
+
+        delete:
+        Delete a specific post_image information
+
+        patch:
+        Update a specific post_image information
+
+    """
     queryset = PostImage.objects.all()
     serializer_class = PostImageSerializer
+    #permission_classes = [IsOwnerIsAdminOrReadOnly]
+    # need a discussion before implementing this.
