737db87ce2a95827fd321d43f637f074216b736d
Stefan Lustenberger <chrott77@gmail.com>
2023-06-30 09:28:01 +0200

    changed some views from patch to post, hidden som puts

--- backend/user_registration/views.py
+++ backend/user_registration/views.py
@@ -56,11 +56,11 @@ class RegisterView(CreateAPIView):
         return Response(status=status.HTTP_201_CREATED)
 
 
-class RegisterValidationView(UpdateAPIView):
+class RegisterValidationView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationValidationSerializer
 
-    def patch(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         user_instance = get_object_or_404(User, email=request.data['email'])
         code_instance = get_object_or_404(UserRegistration, user=user_instance)
         if request.data['code'] == code_instance.code \
