72e3e5e0274cf41a908868cb3585125d5a1edede
Maheshwaran Velusamy <mahes1287@gmail.com>
2023-06-30 11:16:56 +0200

    friend request permission

--- backend/friendrequest/permissions.py
+++ backend/friendrequest/permissions.py
@@ -1,8 +1,30 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerIsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.methods in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == self.request.user
+        if request.user.is_superuser:
+            return True
+        return obj.creating_user == request.user
+
+
+class CanDeleteFriendRequest(permissions.BasePermission):
+    message = "You are not allowed to delete other peoples friend request"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        # Only the sending user can delete the friend request
+        return obj.sending_user == request.user
+
+
+class CanUpdateFriendRequest(permissions.BasePermission):
+    message = "You dont have permission to change status of this friend request"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        # Only the receiving user can update (accept/decline) the friend request
+        return obj.receiving_user == request.user
