857c777d87952bdd3a82d50a46be3f05131dae8c
Stefan Lustenberger <chrott77@gmail.com>
2023-06-30 09:30:22 +0200

    removed an import

--- backend/user_registration/views.py
+++ backend/user_registration/views.py
@@ -3,7 +3,7 @@ import random
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
